e5a4e3211bfc4cf170b155869efd1fba086134e2
SergeyPotemkin <yenon@yandex.ru>
2017-06-01 21:48:56 +0300

    Update ExportDoc.Designer.cs

--- DiagnosisOfRetinal/ExportDoc.Designer.cs
+++ DiagnosisOfRetinal/ExportDoc.Designer.cs
@@ -1,4 +1,16 @@
-﻿namespace DiagnosisOfRetinal
+/*
+    Copyright 2016,2017 Sergey Potemkin
+    This file is part of the DOR.
+   (Diagnosis of Retina)
+
+   DOR is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License v3.0.
+    
+    You should have received a copy of the GNU General Public License v3.0
+    along with DOR.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace DiagnosisOfRetinal
 {
     partial class ExportDoc
     {
